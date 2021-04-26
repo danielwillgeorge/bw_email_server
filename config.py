ad04de7c3dc889c3c90f70efa2409264b4f3cbd5
Daniel George <daniel.george@disney.com>
2021-04-26 07:42:04 -0700

    first commit

---
+++ config.py
@@ -0,0 +1,8 @@
+# The below EMAIL_SERVICE_PROVIDER value may be adjusted to target "SNAILGUN"
+# or "SPENDGRID".
+EMAIL_SERVICE_PROVIDER = "SPENDGRID"
+
+BRIGHTWHEEL_EMAIL_SERVICE = "http://127.0.0.1:8000/email"
+SPENDGRID_URL = "https://bw-interviews.herokuapp.com/spendgrid/send_email"
+SNAILGUN_URL = "https://bw-interviews.herokuapp.com/snailgun/emails"
+SNAILGUN_STATUS_URL = "https://bw-interviews.herokuapp.com/snailgun/emails/"
